a015ccc87fcf1a50528b7fa55ec60e173422d0b2
Paulo A.C. Peixoto <90398620+Spanyk@users.noreply.github.com>
2022-04-30 09:36:36 -0300

    Update program.cs

--- exer 16/program.cs
+++ exer 16/program.cs
@@ -1,49 +1,57 @@
-/ Online C# Editor for free
-// Write, Edit and Run your C# code using C# Online Compiler
-    
 using System;
+using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
-public class HelloWorld
+namespace Exer_16
 {
-    class tp_no 
+    internal class Program
     {
-    public int valor;
-    public tp_no prox;
-    }
+        class tp_no
+        {
+            public int valor;
+            public tp_no prox;
+        }
 
-    static void Insere(ref tp_no t, int v)
-    {
-        tp_no no = new tp_no();
-        no.valor = v;
-        if (t != null)
-            no.prox = t;
+        static void Insere(ref tp_no t, int v)
+        {
+            tp_no no = new tp_no();
+            no.valor = v;
+            if (t != null)
+                no.prox = t;
             t = no;
-    }
-    
-    static tp_no Remove(ref tp_no t)
-    {
-        tp_no no = null;
-        if (t != null)
-        {
-            no = t;
-            t = t.prox;
-            no.prox = null;
         }
-        return no;
-    }
-    
-    public static void Main(string[] args)
-    {
-        tp_no topo = null;
-        
-        for(int i = 0; i < 5; i++)
+
+        static tp_no Remove(ref tp_no t)
         {
-            Console.Write("n°: ");
-            int num = int.Parse(Console.ReadLine());
-            Insere(ref tp_no topo, num);
+            tp_no no = null;
+            if (t != null)
+            {
+                no = t;
+                t = t.prox;
+                no.prox = null;
+            }
+            return no;
         }
-        
-        Console.ReadKey();
+
+        public static void Main(string[] args)
+        {
+            tp_no topo = null;
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.Write("n°: ");
+                int num = int.Parse(Console.ReadLine());
+                Insere(ref topo, num);
+            }
+            for(int i = 0; i < 5; i++)
+            {
+                var valor = Remove(ref topo);
+                Console.WriteLine(valor);
+            }
+
+            Console.ReadKey();
+        }
     }
 }
