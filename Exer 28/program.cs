2bf7adb8a7579c72e9d6c688c40979f538cf50d3
Paulo Augusto <90398620+Spanyk@users.noreply.github.com>
2022-06-07 09:07:57 -0300

    Update program.cs

--- Exer 28/program.cs
+++ Exer 28/program.cs
@@ -8,50 +8,64 @@ namespace Exer_28
     {
         const int N = 5;
 
-        class Tp_no
+        class Sc_no // Sem tratamento de colisão
         {
-            public int nota;
-            public string nome, email;
-        }
-        
-        static int hash(int c)
-        {
-            return (c % N) + 1;
+            public int idade;
+            public string nome, sexo;
         }
 
-        static void criaNos(Tp_no[] V)
+        static int Hash(int c)
+        {
+            return (c % N);
+        }
+        static void Insere(Sc_no[] v, int age, string nm, string sex)
+        {
+            
+            int pos = Hash(age);
+            v[pos].idade = age;
+            v[pos].nome = nm;
+            v[pos].sexo = sex;
+        }
+        static int Busca(int age)
+        {
+            int pos = Hash(age); ;
+            return pos;
+
+        }
+        static void criaNos(Sc_no[] V)
         {
             int i;
-            for(i =0; i < N; i++)
+            for (i = 0; i < N; i++)
             {
-                V[i] = new Tp_no();
+                V[i] = new Sc_no();
             }
         }
-        static void InserirLinear(Tp_no[] v, int nt, string nm, string em, ref int q)
+        /*
+        static void InserirLinear(Tp_no[] v, int age, string nm, string sex, ref int q)
         {
-            int pos = hash(nt);
-            while(v[pos].nota != 0)
+            int pos = hash(age);
+            while (v[pos].idade != 0)
             {
                 pos++;
                 pos = pos % N;
                 q += q;
             }
-            v[pos].nota = nt;
+            v[pos].idade = age;
             v[pos].nome = nm;
-            v[pos].email = em;
+            v[pos].sexo = sex;
         }
         static int RecuperarLinear(Tp_no[] v, int nt)
         {
             int pos = hash(nt);
             int cont = 0;
 
-            while(v[pos].nota != nt && cont <= N)
+            while (v[pos].idade != nt && cont <= N)
             {
                 pos++;
                 pos = pos % N;
                 cont++;
             }
-            if(cont <= N)
+            if (cont <= N)
             {
                 return pos;
             }
@@ -59,62 +73,139 @@ namespace Exer_28
             {
                 return -1;
             }
+
         }
-        static void Main(string[] args)
+        */
+        static void funcionalidades(string x) // main 2
         {
-            Tp_no[] Vetor = new Tp_no[N];
+            Sc_no[] Vetor = new Sc_no[N];
             criaNos(Vetor);
-            int op = 0, nota = 0, qtde = 0;
-            string nome, email;
+            int op = 0, idade = 0;
+            string nome, sexo;
 
-            while(op != 4)
+            while (true)
             {
-                Console.Clear();
-                Console.ForegroundColor = ConsoleColor.Green;
-                Console.WriteLine("Menu");
-                Console.WriteLine("1- Insere\n2- Recuperar\n3- Informar\n4- Sair\n");
-                op = Convert.ToInt32(Console.ReadLine());
-                
-                if(op == 1)
-                {
-                    Console.Write(">> Nota: ");
-                    nota = Convert.ToInt32(Console.ReadLine());
-                    Console.Write(">> Nome: ");
-                    nome = Console.ReadLine();
-                    Console.Write(">> Email: ");
-                    email = Console.ReadLine();
 
-                    InserirLinear(Vetor, nota, nome, email, ref qtde);
+            
+                Console.WriteLine("\nEscolha uma opção:");
+                Console.Write(
+                    "[1] Inserir\n" +
+                    "[2] Consultar\n" +
+                    "[3] Alterar\n" +
+                    "[4] Relatar\n" +
+                    "\n>>  "
+                    );
+                op = Convert.ToInt32(Console.ReadLine());
+
+                if (op == 1)
+                {
+                    Console.WriteLine("\n>> Prencha os campos");
+                    Console.Write("Nome: ");
+                    nome = Console.ReadLine();
+                    Console.Write("Idade: ");
+                    idade = Convert.ToInt32(Console.ReadLine());
+                    Console.Write("Sexo: ");
+                    sexo = Console.ReadLine();
+
+                    switch (x)
+                    {
+                        case "1":
+                            Insere(Vetor, idade, nome, sexo);
+                            break;
+
+                        case "2":
+                            break;
+
+                        case "3":
+                            Console.ForegroundColor = ConsoleColor.Red;
+                            Console.WriteLine(">> Gay <<");
+                            Console.ResetColor();
+                            break;
+                    }
                 }
+
                 else if (op == 2)
                 {
-                    Console.Write(">> Nota: ");
-                    nota = Convert.ToInt32(Console.ReadLine());
-                    int pos = RecuperarLinear(Vetor, nota);
-                    
-                    if(pos != -1)
-                    {
-                        Console.WriteLine(">> Nome: " + Vetor[pos].nome);
-                        Console.WriteLine(">> Email: " + Vetor[pos].email);
+                    Console.Write(">> Informe a idade: ");
+                    int idadeBuscada = int.Parse(Console.ReadLine());
 
-                    }
-                    else
-                    {
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.DarkRed;
-                        Console.WriteLine("<<< Não encontrado >>>");
-                        Console.ResetColor();
-                        Console.ForegroundColor = ConsoleColor.Green;
-
-                    }
+                    Busca(idadeBuscada);
                 }
 
-                else if(op == 3)
+                else if (op == 3)
                 {
-                    Console.WriteLine(">> Quantidade de colisões: " + qtde);
-                    Console.ReadKey();
+
                 }
+
+                else if (op == 4)
+                {
+
+                }
+
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(">> Insira uma opção válida <<");
+                    Console.ResetColor();
+                }
+        }
+    }
+        static void Main(string[] args)
+        {
+            int op = 0;
+            Console.WriteLine("Escolha um opção");
+            Console.Write(
+            "[1] Sem tratamento de colisão\n" +
+            "[2] Tratamento de colisão Linear\n" +
+            "[3] Tratamento de colisão com lista encadeada\n" +
+            "\n>> "
+            );
+
+            op = int.Parse(Console.ReadLine());
+            switch (op)
+            {
+                case 1:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(">> Opção escolhida: Sem tratamento <<");
+                    Console.ResetColor();
+                    SemColisao();
+                    break;
+
+                case 2:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(">> Opção escolhida: Com tratamento <<");
+                    Console.ResetColor();
+                    ColisaoLinear();
+                    break;
+
+                case 3:
+                    Console.Clear();
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine(">> Opção escolhida: Colisão encadeada <<");
+                    Console.ResetColor();
+                    ColisaoLinearEncadeada();
+                    break;
             }
+        }
+
+        static void SemColisao()
+        {
+            string x = "1";
+            funcionalidades(x);
+        }
+
+        static void ColisaoLinear()
+        {
+            string x = "2";
+            funcionalidades(x);
+        }
+
+        static void ColisaoLinearEncadeada()
+        {
+            string x = "3";
+            funcionalidades(x);
         }
     }
 }
